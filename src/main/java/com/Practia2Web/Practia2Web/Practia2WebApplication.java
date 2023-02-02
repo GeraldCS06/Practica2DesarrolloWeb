3188939406f9415934d1ab860cdee5bf3052dac0
Gerald0609 <66327871+Gerald0609@users.noreply.github.com>
2023-02-01 21:04:50 -0600

    Avance 1 de la practica 2 de desarollo web

---
+++ src/main/java/com/Practia2Web/Practia2Web/Practia2WebApplication.java
@@ -0,0 +1,13 @@
+package com.Practia2Web.Practia2Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practia2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practia2WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Practia2Web/Practia2Web/Practia2WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practia2Web.Practia2Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practia2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
